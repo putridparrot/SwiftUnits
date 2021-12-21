d0632eb66a189607011ecfda53389f7c04911f8a
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2021-12-21 16:17:10 +0000

    Files in wrong place

--- Tests/SwiftUnitsTests/SwiftUnitsTests.swift
+++ Tests/SwiftUnitsTests/SwiftUnitsTests.swift
@@ -6,6 +6,6 @@ final class SwiftUnitsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftUnits().text, "Hello, World!")
+        //XCTAssertEqual(SwiftUnits().text, "Hello, World!")
     }
 }
